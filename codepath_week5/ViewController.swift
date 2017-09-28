c1c88847424088234fb9b89550d1cfa2e4f0ce28
janechung <jane@coinbase.com>
2017-09-27 20:49:12 -0700

    Initial Commit

---
+++ codepath_week5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codepath_week5
+//
+//  Created by Jaehee Chung on 9/27/17.
+//  Copyright © 2017 Jaehee Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
