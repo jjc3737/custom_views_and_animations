e4eea4f7a6c0e60364690b77e774bd5fa8afd5c8
janechung <jane@coinbase.com>
2017-09-30 16:06:20 -0700

    Implement custom view through code

--- codepath_week5/View Controllers/ViewController.swift
+++ codepath_week5/View Controllers/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	fileprivate var actionView: ActionView!
+	fileprivate let colors = [UIColor.orange, UIColor.white, UIColor.green, UIColor.brown, UIColor.red]
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		actionView = ActionView(frame: CGRect(x: 60, y: 60, width: view.bounds.width - 120, height: 150))
+		actionView.prepare(with: "Change my color", actionTitle: "Ok") {
+			let randIndex = Int(arc4random_uniform(5))
+			self.view.backgroundColor = self.colors[randIndex]
+		}
+		view.addSubview(actionView)
+		
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
